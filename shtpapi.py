560dec05bd43d1ee7eb1f754a75ee0b2a276eb5e
ret7020 <ret7020@gmail.com>
2022-11-28 18:04:46 +0300

    Auth method

--- shtpapi.py
+++ shtpapi.py
@@ -7,10 +7,14 @@ class API:
         self.GET = requests.get
         self.POST = requests.post
         self.URL = "http://localhost:8080"  # local api
+        self.authed = False
 
     def direct_req(self, endpoint: str, data: dict, req_type) -> dict:
-        data = req_type(self.URL + endpoint, data=data).json()
-        return data
+        data = req_type(self.URL + endpoint, json=data, headers={"Authorization": f"Bearer {self.authed}"})
+        if data.status_code == 200:
+            return data.json()
+        else:
+            return {"status": False, "req_failed": data.status_code}
 
     def get_user(self, user_id: int, as_obj: bool = False) -> dict:
         res = self.direct_req(f"/users/{user_id}/info", {}, self.GET)
@@ -18,3 +22,12 @@ class API:
             return User(res)
         else:
             return res
+    
+    def auth(self, login: str, password: str, auth_api: bool = True):
+        auth_data = self.direct_req("/auth/login", {"email": login, "password": password}, self.POST)
+        if not "req_failed" in auth_data:
+            if auth_api:
+                self.authed = auth_data['accessToken']
+            return auth_data['accessToken']
+        else:
+            return False
--- test.py
+++ test.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     API = api()
     json = API.get_user(1)
     obj = API.get_user(1, as_obj=True)
-    print(obj)
+    auth = API.auth("EMAIL", "PASSWORD")
+    print(auth)
